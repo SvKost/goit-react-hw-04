dadec1e111d8f2f2df11a777c6f7aebe58a173e5
SvKost <skost0634610557@gmail.com>
2024-03-15 23:25:31 +0200

    axios, fetch function added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,42 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
+
 import "./App.css";
 import SearchBar from "./components/SearchBar/SearchBar";
+import ImageGallery from "./components/ImageGallery/ImageGallery";
 
-function App() {
+const API_URL = "https://api.unsplash.com/search/photos";
+const API_KEY = "DNjz_WmDfP4R6Lavo-0pe3nO3flY5cDFdXY_l6ZaDV0";
+const IMAGES_PER_PAGE = 20;
+
+function App({ query }) {
+  const [photos, setPhotos] = useState([]);
+  useEffect(() => {
+    const getImages = async () => {
+      try {
+        const response = await axios.get(
+          `${API_URL}?query=${query}&page=1&per_page=${IMAGES_PER_PAGE}&client_id=${API_KEY}`
+        );
+        setPhotos(response.data);
+        console.log(response.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    getImages();
+  }, []);
+
   const handleSearch = (searchQuery) => {
     console.log(searchQuery);
   };
 
-  return <SearchBar onSearch={handleSearch} />;
+  return (
+    <div>
+      <SearchBar onSearch={handleSearch} />;
+      <ImageGallery />;
+    </div>
+  );
 }
 
 export default App;
