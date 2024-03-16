2bcf0903ddadd6b1f95f1f75682e56bcf0c262fb
SvKost <skost0634610557@gmail.com>
2024-03-16 22:36:32 +0200

    Loading added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
-
 import "./App.css";
 import SearchBar from "./components/SearchBar/SearchBar";
 import ImageGallery from "./components/ImageGallery/ImageGallery";
+import { ThreeDots } from "react-loader-spinner";
 
 const API_URL = "https://api.unsplash.com/search/photos";
 const API_KEY = "DNjz_WmDfP4R6Lavo-0pe3nO3flY5cDFdXY_l6ZaDV0";
@@ -11,14 +11,21 @@ const API_KEY = "DNjz_WmDfP4R6Lavo-0pe3nO3flY5cDFdXY_l6ZaDV0";
 
 function App() {
   const [images, setImages] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     async function getImages() {
-      const response = await axios.get(
-        "https://api.unsplash.com/photos?client_id=DNjz_WmDfP4R6Lavo-0pe3nO3flY5cDFdXY_l6ZaDV0"
-      );
-      setImages(response.data);
-      console.log(response);
+      try {
+        setIsLoading(true);
+        const response = await axios.get(
+          "https://api.unsplash.com/photos?client_id=DNjz_WmDfP4R6Lavo-0pe3nO3flY5cDFdXY_l6ZaDV0"
+        );
+        setImages(response.data);
+      } catch (err) {
+        //TODO:
+      } finally {
+        setIsLoading(false);
+      }
     }
 
     getImages();
@@ -30,6 +37,18 @@ function App() {
 
   return (
     <div>
+      {isLoading && (
+        <ThreeDots
+          visible={true}
+          height="80"
+          width="80"
+          color="#4fa94d"
+          radius="9"
+          ariaLabel="three-dots-loading"
+          wrapperStyle={{}}
+          wrapperClass=""
+        />
+      )}
       <ul>
         {images !== null &&
           images.map((image) => {
