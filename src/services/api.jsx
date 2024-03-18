a0b67a5cbe565f9e440f9be4f5f668828abfa2d0
SvKost <skost0634610557@gmail.com>
2024-03-18 13:21:31 +0200

    Update api.jsx

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -1,10 +1,13 @@
 import axios from "axios";
 
-const API_URL = "https://api.unsplash.com/photos";
+const API_URL = "https://api.unsplash.com/search";
 const API_KEY = "DNjz_WmDfP4R6Lavo-0pe3nO3flY5cDFdXY_l6ZaDV0";
 // const IMAGES_PER_PAGE = 20;
 
-export const fetchImages = async () => {
-  const response = axios.get(`${API_URL}?client_id=${API_KEY}`);
-  return response;
+export const fetchImagesByQuery = async (query) => {
+  const response = await axios.get(
+    `${API_URL}/photos?client_id=${API_KEY}?query=${query}`
+  );
+
+  return response.data;
 };
